4fcc62f4312e20a9fdfadf8949579320f1f96ad4
WilliamTsou818 <cccc1589@gmail.com>
2021-07-26 15:16:46 +0800

    feat: add redis in get /api/blogs route

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -13,8 +13,23 @@ module.exports = app => {
   })
 
   app.get('/api/blogs', authenticate, async (req, res) => {
+    // include redis
+    const redis = require('redis')
+    const redisUrl = 'redis://127.0.0.1:6379'
+    const client = redis.createClient(redisUrl)
+    // promisify cached data
+    const util = require('util')
+    client.get = util.promisify(client.get)
+
+    // check if cached data exist
+    const cachedBlogs = await client.get(req.user.id)
+    if (cachedBlogs) {
+      return res.send(JSON.parse(cachedBlogs))
+    }
+
     const blogs = await Blog.find({ _user: req.user.id })
     res.send(blogs)
+    client.set(req.user.id, JSON.stringify(blogs))
   })
 
   app.post('/api/blogs', authenticate, async (req, res) => {
