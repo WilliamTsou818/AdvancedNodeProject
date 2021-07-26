d5fa2b28a79bfc9c242c7b8789a954d51879e96b
WilliamTsou818 <cccc1589@gmail.com>
2021-07-26 18:19:48 +0800

    feat: add cache expiration

--- services/cache.js
+++ services/cache.js
@@ -34,6 +34,6 @@ mongoose.Query.prototype.exec = async function () {
   }
 
   const result = await exec.apply(this, arguments)
-  client.set(key, JSON.stringify(result))
+  client.set(key, JSON.stringify(result), 'EX', 10)
   return result
 }
