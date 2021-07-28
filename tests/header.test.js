272e5945286a46cd83b9dddcb8e239c6af428530
WilliamTsou818 <cccc1589@gmail.com>
2021-07-28 17:46:27 +0800

    refactor: add getContentsOf method in test helpers

--- tests/header.test.js
+++ tests/header.test.js
@@ -20,7 +20,7 @@ afterAll(() => {
 })
 
 test('the header has the correct text', async () => {
-  const text = await page.$eval('a.brand-logo', el => el.innerHTML)
+  const text = await page.getContentsOf('a.brand-logo')
   expect(text).toEqual('Blogster')
 })
 
@@ -33,6 +33,6 @@ test('clicking login starts oauth flow', async () => {
 test('When signed in, shows logout button', async () => {
   await page.login()
 
-  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML)
+  const text = await page.getContentsOf('a[href="/auth/logout"]')
   expect(text).toEqual('Logout')
 })
