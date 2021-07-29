6ae1ef24268ebd77756e920205dd798dd9a03ff7
WilliamTsou818 <cccc1589@gmail.com>
2021-07-29 14:38:19 +0800

    feat: add code to test blog create feature

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -36,6 +36,31 @@ describe('When logged in', async () => {
     expect(label).toEqual('Blog Title')
   })
 
+  describe('And using valid inputs', async () => {
+    beforeEach(async () => {
+      await page.type('.title input', 'My Test Title')
+      await page.type('.content input', 'My Test Content')
+      await page.click('form button')
+    })
+
+    test('Submitting takes user to review screen', async () => {
+      const text = await page.getContentsOf('h5')
+
+      expect(text).toEqual('Please confirm your entries')
+    })
+
+    test('Submitting then saving adds blog to index page', async () => {
+      await page.click('button.green')
+      await page.waitFor('.card')
+
+      const title = await page.getContentsOf('.card-title')
+      const content = await page.getContentsOf('p')
+
+      expect(title).toEqual('My Test Title')
+      expect(content).toEqual('My Test Content')
+    })
+  })
+
   describe('And using invalid inputs', async () => {
     beforeEach(async () => {
       await page.click('form button')
